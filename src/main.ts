24a4105b581ee6e1c9914e73f811acc724eed6fc
longsiru <sirulong89@gmail.com>
2023-04-19 23:36:24 +0900

    connect mysql step

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,9 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+//1.npm install --save @nestjs/typeorm typeorm mysql2
+//2. app.module配置数据库连接
+//3.配置entity实体
+//4.在控制器对应的module中配置model。
+//5.在service里面使用@injectrepository 获取数据库，odel实现操作数据库。

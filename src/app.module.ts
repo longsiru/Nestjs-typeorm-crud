a33b2bd3cf206b744ae6e90d6372512c106a464d
longsiru <sirulong89@gmail.com>
2023-04-19 23:36:43 +0900

    connect mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//配置数据库链接
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'nest',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], //__dirname报错时，npm i -D @types/node
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

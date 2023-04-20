d262e430fb858220a5b1d529d53445f05e42d3fc
longsiru <sirulong89@gmail.com>
2023-04-20 18:35:08 +0900

    Update nav.controller.ts

--- src/module/admin/controller/nav/nav.controller.ts
+++ src/module/admin/controller/nav/nav.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get } from '@nestjs/common';
 import { NavService } from '../../../../service/nav/nav.service';
+import { json } from 'stream/consumers';
 
 @Controller('admin/nav')
 export class NavController {
   constructor(private navService: NavService) {}
 
+  //查找数据
   @Get()
   async index() {
     return await this.navService.findAll();
+  }
+
+  //增加数据，暂时用get，真正做项目的时候用post
+  /*
+  @Get('add')
+  async add() {
+    return await this.navService.add();
+  }
+  */
+
+  //用json来添加。
+  @Get('add')
+  async add() {
+    return await this.navService.add({
+      title: 'luntan2',
+      url: 'ccc.ccc.com',
+      description: 'c',
+      status: 1,
+    });
+  }
+
+  //update
+  @Get('update')
+  async update() {
+    return await this.navService.update({ id: 4 }, { title: 'nestjsclass' });
+  }
+
+  //delete
+  @Get('remove')
+  async remove() {
+    return await this.navService.remove({ id: 3 });
   }
 }
